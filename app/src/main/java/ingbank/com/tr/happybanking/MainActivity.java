79e037c8b7dde1b1d56ac107be531b3f2c0744b3
burcuturkmen <burcu.turkmen@ingbank.com.tr>
2015-11-20 18:42:53 +0200

    Öğretmen ekranı ve anasayfa eklendi.

--- app/src/main/java/ingbank/com/tr/happybanking/MainActivity.java
+++ app/src/main/java/ingbank/com/tr/happybanking/MainActivity.java
@@ -1,5 +1,7 @@
 package ingbank.com.tr.happybanking;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,24 +10,40 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
+    private Button btnDemo1;
+    private Button btnDemo2;
+    private Button btnDemo3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        //Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main);
+        createComponent();
+
+
+    }
+
+    private void createComponent() {
+        btnDemo1=(Button)findViewById(R.id.btnDemo1);
+        btnDemo1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),ActivityDemo1.class);
+                startActivity(intent);
             }
         });
+        btnDemo2=(Button)findViewById(R.id.btnDemo2);
+        btnDemo3=(Button)findViewById(R.id.btnDemo3);
     }
 
     @Override
